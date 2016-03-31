9f8a4397bf065e5a7e0626acc259341064873a5a
Gaurav Gupta <guptgaurav@gmail.com>
2016-03-31 17:05:43 -0400

    updating code

--- Vagrantfile
+++ Vagrantfile
@@ -5,9 +5,10 @@ Vagrant.configure("2") do |config|
       mgmt_config.vm.box = "ubuntu/trusty64"
       mgmt_config.vm.hostname = "mgmt"
       mgmt_config.vm.network :private_network, ip: "10.0.15.10"
+      mgmt_config.ssh.forward_agent = true
       mgmt_config.vm.provider "virtualbox" do |vb|
         vb.memory = "256"
       end
-      mgmt_config.vm.provision :shell, path: "bootstrap-mgmt.sh"
+      mgmt_config.vm.provision :shell, path: "bootstrap-mgmt.sh", privileged: false
   end
 end
--- bootstrap-mgmt.sh
+++ bootstrap-mgmt.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-# installs the packages 
-apt-get -y install software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
-apt-get -y install ansible
-apt-get -y install git
+# installs the packages
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+sudo apt-get -y install git
+sudo apt-get -y install python-pip
+sudo pip install boto
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
+# su - vagrant
+# todo - git clone the file I really want with ansible details and code repo
+# mkdir code-lab
+# mkdir code-lab/inventory
+# cd code-lab/inventory
+# wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py
+# wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini
+# chmod +x ec2.py
